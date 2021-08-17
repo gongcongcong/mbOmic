2bacd4dde1ddb0c1fa51dd40c28de99d410dd9d9
Congcong Gong <991624249@qq.com>
2021-08-17 22:46:06 +0800

    remove mbOmic.Rchek

--- mbOmic.Rcheck/00_pkg_src/mbOmic/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(mbOmic)
-
-test_check("mbOmic")
--- mbOmic.Rcheck/00_pkg_src/mbOmic/tests/testthat/test-corr.R
+++
@@ -1,7 +0,0 @@
-test_that("multiplication works", {
-  expect_equal(corr.test(data.frame(a=1:3,b=1:3),
-                         data.frame(c=2:4,d=2:4))$r,
-               matrix(1,nrow=2,ncol=2,
-                      dimnames = list(c('a','b'),
-                                     c('c','d'))))
-})
